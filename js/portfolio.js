d9161f5f7e91317981a2f49b606a84dc723675e6
Anass EL <evolutionstudio3000@gmail.com>
2023-08-21 14:57:16 +0200

    New commit August 21/2023

--- js/portfolio.js
+++ js/portfolio.js
@@ -146,30 +146,57 @@ function showSlides4(n) {
   dots[slideIndex4 - 1].className += " active";
 }
 
-// let slideIndex5 = 1;
-// showSlides5(slideIndex5);
 
-// function plusSlides5(n) {
-//   showSlides5(slideIndex5 += n);
-// }
-// function currentSlide5(n) {
-//   showSlides5(slideIndex5 = n);
-// }
+let slideIndex5 = 1;
+showSlides5(slideIndex5);
+
+function plusSlides5(n) {
+  showSlides5(slideIndex5 += n);
+}
+function currentSlide5(n) {
+  showSlides5(slideIndex5 = n);
+}
+
+function showSlides5(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides5");
+  let dots = document.getElementsByClassName("demo5");
+
+  if (n > slides.length) { slideIndex5 = 1 }
+  if (n < 1) { slideIndex5 = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex5 - 1].style.display = "block";
+  dots[slideIndex5 - 1].className += " active";
+}
 
 
-// function showSlides5(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("slides5");
-//   let dots = document.getElementsByClassName("demo5");
 
-//   if (n > slides.length) { slideIndex5 = 1 }
-//   if (n < 1) { slideIndex5 = slides.length }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex5 - 1].style.display = "block";
-//   dots[slideIndex5 - 1].className += " active";
-// }
+let slideIndex6 = 1;
+showSlides6(slideIndex6);
+function plusSlides6(n) {
+  showSlides6(slideIndex6 += n);
+}
+function currentSlide6(n) {
+  showSlides6(slideIndex6 = n);
+}
+function showSlides6(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides6");
+  let dots = document.getElementsByClassName("demo6");
+
+  if (n > slides.length) { slideIndex6 = 1 }
+  if (n < 1) { slideIndex6 = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex6 - 1].style.display = "block";
+  dots[slideIndex6 - 1].className += " active";
+}
